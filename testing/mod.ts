3f8c784cf54096f3bbffbd8321e56e001aa3e725
melike2d <gino@dimensional.fun>
2021-08-08 12:39:13 -0700

    :white_check_mark: update tests

---
+++ testing/mod.ts
@@ -0,0 +1,189 @@
+import { startBot, cache, ws, Embed, CreateMessage, sendMessage, getUser } from "https://deno.land/x/discordeno/mod.ts";
+import { ms } from "https://raw.githubusercontent.com/denolib/ms/master/ms.ts";
+
+
+import { Lavalink } from "../deps.ts";
+import { DiscordVoiceServer, DiscordVoiceState, Cluster, constants, Player } from "../mod.ts";
+import { Queue } from "./queue.ts";
+import testsConfig from "./tests.config.ts";
+
+constants.clientName = "lavadeno-testing"
+constants.useFilters = true;
+
+const cluster = new Cluster({
+    nodes: [testsConfig.node],
+    sendGatewayPayload: (id, payload) => {
+        const shard = cache.guilds.get(id)?.shardId;
+        if (shard != null) ws.sendShardMessage(shard, payload);
+    },
+    userId: 870267613635309618n
+})
+
+const queues: Map<bigint, Queue> = new Map();
+
+cluster.on("nodeDisconnect", (node, { code, reason }) => {
+    console.log(`[bot] (node ${node.id}) disconnected, code=${code}, reason=${reason ? `"${reason}"` : "unknown"}`)
+});
+
+cluster.on("nodeError", (_, error) => {
+    void error;
+});
+
+cluster.on("nodeDebug", (node, message) => {
+    console.debug(`[music] (node ${node.id}) ${message}`)
+});
+
+cluster.on("nodeConnect", (node, reconnect) => {
+    console.log(`[music] (node ${node.id}) ${reconnect ? "re" : ""}connected to node.`);
+    node.rest.routePlanner
+        .status()
+        .then(status => console.dir(status));
+});
+
+startBot({
+    token: testsConfig.token,
+    intents: ["Guilds", "GuildMessages", "GuildVoiceStates"],
+    eventHandlers: {
+        ready: () => {
+            console.log("[bot] ready");
+            cluster.init();
+        },
+        raw: data => {
+            switch (data.t) {
+                case "VOICE_SERVER_UPDATE":
+                case "VOICE_STATE_UPDATE":
+                    cluster.handleVoiceUpdate(data.d as (DiscordVoiceState | DiscordVoiceServer));
+                    break;
+            }
+        },
+        messageCreate: async message => {
+            if (message.isBot || !message.content.startsWith("!")) {
+                return;
+            }
+
+            const [command, ...args] = message.content
+                .slice(1)
+                .trim()
+                .split(/\s+/g);
+
+            switch (command.toLowerCase()) {
+                case "join": {
+                    let player = cluster.players.get(message.guildId);
+                    if (player?.connected) {
+                        return message.reply(embed("A player for this guild already exists"), false)
+                    }
+
+                    const vc = message.guild?.voiceStates?.get(message.authorId);
+                    if (!vc?.channelId) {
+                        return message.reply(embed("You need to join a voice channel!"), false)
+                    }
+
+                    player ??= cluster.createPlayer(message.guildId);
+                    player.connect(vc.channelId, { deafen: true });
+                    createQueue(player, message.channelId);
+
+                    return message.reply(embed(`Connected to <#${vc.channelId}>`), false);
+                }
+                case "nowplaying": {
+                    const player = cluster.players.get(message.guildId);
+                    if (!player?.connected) {
+                        return message.reply(embed("A player for this guild doesn't exist."), false)
+                    }
+
+                    const current = queues.get(player.guildId)?.current;
+                    if (!current) {
+                        return message.reply(embed("Nothing is currently playing."), false);
+                    }
+
+                    const requester = current.requester && await getUser(current.requester);
+                    return message.reply(embed(`**Duration:** ${ms(current.length, { long: true })}`, {
+                        title: current.title,
+                        url: current.uri,
+                        footer: requester
+                            ? { text: `It was requested by ${requester.username}#${requester.discriminator}` }
+                            : undefined,
+                        thumbnail: { url: current.thumbnail }
+                    }), false)
+                }
+                case "nightcore": {
+                    const player = cluster.players.get(message.guildId);
+                    if (!player?.connected) {
+                        return message.reply(embed("A player for this guild doesn't exist."), false)
+                    }
+
+                    player.filters.timescale = player.filters.timescale?.rate !== 1
+                        ? { rate: 1.09, pitch: 1.125, speed: 1 }
+                        : { rate: 1, pitch: 1, speed: 1 }
+
+                    player.setFilters();
+
+                    return message.reply(embed(`${player.filters.timescale?.pitch === 1 ? "Disabled" : "Enabled"} **nightcore**!`), false);
+                }
+                case "play": {
+                    const vc = message.guild?.voiceStates?.get(message.authorId);
+                    if (!vc?.channelId) {
+                        return message.reply(embed("You need to join a voice channel!"), false)
+                    }
+
+                    let player = cluster.players.get(message.guildId);
+                    if (player && player.channelId !== vc.channelId) {
+                        return message.reply(embed(`Join <#${player.channelId}> bozo`), false);
+                    }
+
+                    const query = args.join(" ");
+                    const results = await cluster.rest.loadTracks(/^https?:\/\//.test(query)
+                        ? query
+                        : `ytsearch:${query}`);
+
+                    let tracks: Lavalink.Track[] = [], msg = "";
+                    switch (results.loadType) {
+                        case "LOAD_FAILED":
+                        case "NO_MATCHES":
+                            return message.reply(embed("uh oh something went wrong"), false);
+                        case "PLAYLIST_LOADED":
+                            tracks = results.tracks;
+                            msg = `Queued playlist [**${results.playlistInfo.name}**](${query}), it has a total of **${tracks.length}** tracks.`;
+                            break
+                        case "TRACK_LOADED":
+                        case "SEARCH_RESULT": {
+                            const [track] = results.tracks;
+                            tracks = [track];
+                            msg = `Queued [**${track.info.title}**](${track.info.uri})`;
+                            break;
+                        }
+                    }
+
+                    if (!player?.connected) {
+                        player ??= cluster.createPlayer(message.guildId);
+                        await player.connect(vc.channelId, { deafen: true });
+                    }
+
+                    await message.reply(embed(msg), false);
+
+                    const queue = queues.get(player.guildId) ?? createQueue(player, message.channelId);
+
+                    queue.add(tracks, message.authorId);
+                    if (!queue.started) {
+                        queue.start();
+                    }
+                }
+            }
+        }
+    }
+});
+
+function createQueue(player: Player, channelId: bigint): Queue {
+    const queue = new Queue(player);
+    queues.set(player.guildId, queue);
+
+    queue.on("trackStart", song => {
+        sendMessage(channelId, embed(`Now playing [**${song.title}**](${song.uri}) <@${song.requester}>`));
+    });
+
+    return queue;
+}
+
+function embed(description: string, other: Omit<Embed, "description"> = {}): CreateMessage {
+    other.color ??= 0xfff269
+    return { embeds: [{ description, ...other }] };
+}
---
+++ testing/queue.ts
@@ -0,0 +1,197 @@
+import { EventEmitter, Lavalink } from "../deps.ts";
+import { fromSnowflake, Player, Snowflake } from "../mod.ts";
+import { Song } from "./song.ts";
+
+export class Queue extends EventEmitter<QueueEvents> {
+    /**
+     * The tracks that are in this queue.
+     */
+    readonly tracks: Song[];
+
+    /**
+     * The player for this queue.
+     */
+    readonly player: Player;
+
+    /**
+     * The previously played songs.
+     */
+    previous: Song[];
+
+    /**
+     * Whether this queue has been started or not.
+     */
+    started: boolean;
+
+    /**
+     * The currently playing song.
+     */
+    current?: Song;
+
+    /**
+     * The current length of the queue.
+     */
+    length: number;
+
+    /**
+     * The type of loop that is occurring.
+     */
+    private _loop?: "song" | "queue";
+
+    /**
+     * @param player
+     */
+    constructor(player: Player) {
+        super();
+
+        this.player = player.on("trackEnd", (_, reason) => {
+            if (!["REPLACED"].includes(reason)) {
+                this.emit("trackEnd", this.current!);
+                if (this._loop === "song") {
+                    this.tracks.unshift(this.current!);
+                } else {
+                    this.previous.unshift(this.current!);
+                }
+
+                this._next();
+                if (!this.current) {
+                    if (this._loop === "queue") {
+                        this.tracks.push(...this.previous.reverse());
+                        this.previous = [];
+                        return this.start();
+                    }
+
+                    this.emit("finished");
+                    return;
+                }
+
+                return this.player.play(this.current.track);
+            }
+        })
+            .on("trackStart", () => this.emit("trackStart", this.current!));
+
+        this.tracks = [];
+        this.previous = [];
+        this.started = false;
+        this.length = 0;
+    }
+
+    /**
+     * The current type of loop that is occurring.
+     */
+    get loopType(): "song" | "queue" | undefined {
+        return this._loop;
+    }
+
+    /**
+     * Skips the current song and returns the new playing one.
+     * @since 2.0.1
+     */
+    async skip(): Promise<Song | undefined> {
+        await this.player.stop();
+        return this.current;
+    }
+
+    /**
+     * Start the queue.
+     * @since 1.0.0
+     */
+    async start(): Promise<boolean> {
+        if (!this.current) {
+            this._next();
+        }
+
+        if (!this.current) {
+            return false;
+        }
+
+        await this.player.play(this.current.track);
+
+        return this.started = true;
+    }
+
+    /**
+     * Add songs to the queue.
+     * @param songs The song(s) to add.
+     * @param requester The user that requested this song.
+     * @since 1.00.
+     */
+    add(songs: Addable | Array<Addable>, requester?: Snowflake | { id: Snowflake }): number {
+        const requesterId = requester
+            ? fromSnowflake(typeof requester === "object" ? requester.id : requester)
+            : null;
+
+        for (const song of (Array.isArray(songs) ? songs : [songs])) {
+            let toAdd: Song | null = null;
+            if (song instanceof Song) {
+                toAdd = song;
+            } else if (typeof song === "object") {
+                toAdd = new Song(song, requesterId);
+            }
+
+            if (toAdd) {
+                this.tracks.push(toAdd);
+                this.length++;
+            }
+        }
+
+        return this.length;
+    }
+
+    emit<E extends keyof QueueEvents>(event: E, ...args: QueueEvents[E]): Promise<void> {
+        // if (!event.startsWith("_")) {
+        //     const _event = event === "finished" ? "queueFinished" : event;
+        //     if (this.player.listenerCount(_event)) {
+        //         this.player.emit(_event, this, ...args);
+        //     }
+        // }
+
+        return super.emit(event, ...args);
+    }
+
+    /**
+     * Loop the track or queue.
+     * @param type
+     * @since 2.0.1
+     */
+    loop(type: "queue" | "song"): Queue {
+        this._loop = this._loop === type ? undefined : type;
+        return this;
+    }
+
+    /**
+     * Sort the queued songs.
+     * @param predicate
+     * @since 1.0.0
+     */
+    sort(predicate?: (a: Song, b: Song) => number): Array<Song> {
+        return this.tracks.sort(predicate);
+    }
+
+    /**
+     * Shuffle all the tracks in the queue.
+     * @since 1.0.0
+     */
+    shuffle(): void {
+        for (let i = this.tracks.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.tracks[i], this.tracks[j]] = [this.tracks[j], this.tracks[i]];
+        }
+    }
+
+    /**
+     * Get the next song in the queue.
+     * @private
+     */
+    private _next() {
+        return (this.current = this.tracks.shift());
+    }
+}
+
+export type Addable = Lavalink.Track | Song;
+
+export type QueueEvents = {
+    "trackStart": [song: Song];
+    "trackEnd": [song: Song];
+    "finished": [];
+}
