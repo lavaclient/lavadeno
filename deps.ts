456b6260910d788debb63f750530a1c47889f5ae
melike2d <real.melike2d@gmail.com>
2022-10-12 16:34:49 -0700

    chore: remove old stdlib import

--- deps.ts
+++ deps.ts
@@ -1,6 +1,3 @@
 export * as Lavalink from "https://deno.land/x/lavalink_types@2.0.6/mod.ts";
 export * from "https://deno.land/x/event@2.0.0/mod.ts";
 export * as pogsockets from "https://deno.land/x/pogsocket@2.0.0/mod.ts";
-
-/* standard library */
-export { OpCode as WebsocketOpCode } from "https://deno.land/std@0.104.0/ws/mod.ts";
